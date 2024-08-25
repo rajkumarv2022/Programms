da68d58f8c3fa7fa76a6a06a9d2bb90476af12f0
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-08-25 15:53:19 +0530

    commited

---
+++ n_times_pow_of_number.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    
+    public static void main(String[] args) {
+        int n=4;
+        int pow=3;
+        int ans=n;
+        for(int i=2;i<=pow;i++)
+        ans=ans*n;
+        System.out.print(ans);
+    }
+}
