ae1b740ece60f5c5ab4d3960a6483fb63a733a96
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-09-13 20:20:06 +0530

    commited

---
+++ New folder/FactorialofNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FactorialofNumber {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter number : ");
+
+        int num=scan.nextInt();
+
+        int fact=1;
+
+        for(int i=1;i<=num;i++)
+        {
+            fact=fact*i; 
+        }
+
+        System.out.println("The factorial ofa number is : "+fact);
+
+    }
+}
