9b69445e200abed3f2238f4088328e56bdbd2c5d
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-09-21 18:33:13 +0530

    commited

---
+++ New folder/CPboating.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class CPboating{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+
+        int arr[]={1,0,0,0,0,0,0,1};
+
+        int i=0;
+        int j=0;
+
+        while(j<arr.length)
+        {
+            if(arr[j]==1)
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+            }
+            j++;
+        }
+
+        for(int k:arr)
+        {
+            System.out.print(k+" ");
+        }
+
+    }
+}
